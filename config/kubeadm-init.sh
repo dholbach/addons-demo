5a0bdcc39d925fd6f57db0d430cde72ea285b165
Daniel Holbach <daniel@weave.works>
2019-11-25 17:20:32 +0100

    First attempt at integrating Jeff's kube-proxy operator
    	https://github.com/johnsonj/addon-operators/tree/kubeproxy/kubeproxy

--- config/kubeadm-init.sh
+++ config/kubeadm-init.sh
@@ -24,8 +24,8 @@ sed "s/ETH1_IP/${eth1_ip}/g;
     "$CLUSTER_CONFIG".template > "$CLUSTER_CONFIG"
 
 stat "$KUBECONFIG" || \
-kubeadm init --config="$CLUSTER_CONFIG" && \
-kubeadm init phase addon kube-proxy --config=<(sed "/AddonInstaller:/d" "$CLUSTER_CONFIG") && \
+kubeadm init --ignore-preflight-errors --config="$CLUSTER_CONFIG" && \
+#kubeadm init phase addon kube-proxy --config=<(sed "/AddonInstaller:/d" "$CLUSTER_CONFIG") && \
 kubeadm init phase addon coredns --config=<(sed "/AddonInstaller:/d" "$CLUSTER_CONFIG")
 
 
